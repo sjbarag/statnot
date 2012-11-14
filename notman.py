8eca77593a6a63192189bb8e73a853c67f198cd0
Sean Barag <sjbarag@gmail.com>
2012-11-14 14:32:00 -0500

    Hard-code a timeout of 3 seconds for now
    This is merely to ensure things work.
    
    Should I be handling timeouts at all?  An updated notification can
    just be modified.

--- notman.py
+++ notman.py
@@ -296,7 +296,7 @@ class NotificationFetcher(dbus.service.Object):
         add_notification( [app_name.strip(), notification_id,
                           summary.strip(), body.strip(),
                           int(expire_timeout) / 1000.0, app_icon] )
-        ibd.make_notif( summary.strip(), body.strip(), time.strftime("%d %b %Y %H:%M:%S", time.localtime()), app_icon )
+        ibd.make_notif( summary.strip(), body.strip(), time.strftime("%d %b %Y %H:%M:%S", time.localtime()), icon=app_icon, timeout=3000)
         print "made notif"
         return notification_id
 
